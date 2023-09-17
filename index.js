4857f3f33e6efff20100cf2253718cd5f657a2da
fybilal <101524927+billispro@users.noreply.github.com>
2023-09-17 12:02:21 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+
+const app = express();
+
+app.use('',express.static(path.join(__dirname, 'public')));
+
+
+app.get('/auth/discord', (request, response) => {
+	return response.sendFile('dashboard.html', { root: '.' });
+});
+
+const port = '3000';
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
